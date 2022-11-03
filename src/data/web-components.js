5ed280c143b3aaa3c3f53362aad2871bb60bd539
Lanplica <rosenkranz723@gmail>
2022-11-03 18:15:54 +0900

    방송장비_웹캠 추가(#12)

--- src/data/web-components.js
+++ src/data/web-components.js
@@ -2,36 +2,101 @@ export const webcomponentsCollection = {
 	name: "장비",
 	areas: [
 		{
-			name: "카메라",
+			name: "실내용",
 			skills: [
 				{
-					name: "Custom Elements",
+					name: "웹캠",
 					description: {
-						text: "Learn how to create new HTML tags with Custom Elements.",
+						text: "Web과 Camera의 합성어로, 주로 PC와 연결하여 화상 데이터를 네트워크 상에 스트리밍할 목적으로 개발된 카메라를 말한다.",
 						links: [
-							["Google Devs - Custom Elements", "https://developers.google.com/web/fundamentals/web-components/customelements"],
-							["MDN - Using Custom Elements", "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements"],
-							["CSS Tricks - Creating a Custom Element from Scratch", "https://css-tricks.com/creating-a-custom-element-from-scratch/"],
-							["Javascript.info - Custom Elements", "https://javascript.info/custom-elements"],
-							["dev.to - An introduction to Custom Elements", "https://dev.to/jamesrweb/an-introduction-to-custom-elements-5327"],
-							["itnext - Introduction to Web Components", "https://itnext.io/introduction-to-web-components-part-i-custom-elements-4de6713cef9d"],
-							["web.dev - Web Components", "https://web.dev/web-components-io-2019/"]
-						]
-					}
-				},
-				{
-					name: "HTML Templates",
-					description: {
-						text: "Learn how to use HTML templates to create flexible templates that can then be used to populate the Shadow DOM.",
-						links: [
-							["MDN - Using Templates & Slots", "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_templates_and_slots"],
-							["Javascript.info - Template Element", "https://javascript.info/template-element"],
-							["MDN - The Content Template element", "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template"]
+							["프리에이티브 AF500FHD", "https://www.coupang.com/vp/products/5267282638?itemId=7498448512&src=1139000&spec=10799999&addtag=400&ctag=5267282638&lptag=AF5411078&itime=20221101224645&pageType=PRODUCT&pageValue=5267282638&wPcid=16672917945289351501072&wRef=lite-life.tistory.com&wTime=20221101224645&redirect=landing&traceid=V0-101-71102f6b35910cae&placementid=&clickBeacon=&campaignid=&contentcategory=&imgsize=&pageid=&deviceid=&token=&contenttype=&subid=tistoryblog&impressionid=&campaigntype=&requestid=&contentkeyword=&subparam=&isAddedCart="],
+							["앱코 APC930U", "https://www.coupang.com/vp/products/1634788413?itemId=2788295532&src=1139000&spec=10799999&addtag=400&ctag=1634788413&lptag=AF5411078&itime=20221101224939&pageType=PRODUCT&pageValue=1634788413&wPcid=16672917945289351501072&wRef=lite-life.tistory.com&wTime=20221101224939&redirect=landing&traceid=V0-101-f5db09344ecd5cfa&placementid=&clickBeacon=&campaignid=&contentcategory=&imgsize=&pageid=&deviceid=&token=&contenttype=&subid=tistoryblog&impressionid=&campaigntype=&requestid=&contentkeyword=&subparam=&isAddedCart="],
+							["앤커 파워컨퍼런스 C300 A3361", "https://www.coupang.com/vp/products/6199153837?itemId=12289074366&src=1139000&spec=10799999&addtag=400&ctag=6199153837&lptag=AF5411078&itime=20221101225140&pageType=PRODUCT&pageValue=6199153837&wPcid=16672917945289351501072&wRef=lite-life.tistory.com&wTime=20221101225140&redirect=landing&traceid=V0-101-9d568a4bbbdd278f&placementid=&clickBeacon=&campaignid=&contentcategory=&imgsize=&pageid=&deviceid=&token=&contenttype=&subid=tistoryblog&impressionid=&campaigntype=&requestid=&contentkeyword=&subparam=&isAddedCart="],
+							["로지텍 C920 HD PRO WEBCAM", "https://prod.danawa.com/info/?pcode=6846133&keyword=%EB%A1%9C%EC%A7%80%ED%85%8D+c920&cate=112810"]
 						]
 					},
+					skills: [
+						{
+							name: "하이엔드",
+							description: {
+								text: "일반 카메라보다 센서 크기를 키워 화질을 높이고, 이에 더해 비교적 큰 렌즈와 수동 조작 기능 등을 추가한 전문가급 디지털카메라",
+								links: [
+									["캐논 파워샷 G7 X Mark II", "https://prod.danawa.com/info/?pcode=3799052"],
+									["파나소닉 루믹스 DMC-LX10", "https://prod.danawa.com/info/?pcode=4703553"],
+									["SONY 사이버샷 DSC-RX100 VI", "https://prod.danawa.com/info/?pcode=6199512"]
+								]
+							}
+						},
+						{
+							name: "미러리스",
+							description: {
+								text: "렌즈 교환, 디스플레이 장치와 라이브 뷰 촬영을 기반으로 하는 형태의 시스템 카메라를 통틀어 부르는 명칭이다.",
+								links: [
+									["SONY 알파 A6400", "https://prod.danawa.com/info/?pcode=7213111"],
+									["캐논 EOS M50", "https://prod.danawa.com/info/?pcode=5976045"],
+									["파나소닉 루믹스 DMC-GX85", "https://prod.danawa.com/info/?pcode=4087856"]
+								]
+							}
+						},
+						{
+							name: "DSLR",
+							description: {
+								text: "디지털 일안(단일 렌즈) 반사식 카메라(Digital Single-Lens Reflex Camera), 줄여서 DSLR 카메라는 카메라의 렌즈를 통해 인식 장치에 맺히는 피사체의 모습과 거울을 통해 뷰파인더로 반사되어 촬영자에게 보이는 모습이 동일한 디지털카메라",
+								links: [
+									["캐논 EOS 5D Mark IV", "https://prod.danawa.com/info/?pcode=3815381&keyword=%EC%BA%90%EB%85%BC+EOS+5D+%EB%A7%88%ED%81%AC+IV+DSLR&cate=12237484"],
+									["니콘 D3500", "https://prod.danawa.com/info/?pcode=6495760&keyword=%EB%8B%88%EC%BD%98+D3500&cate=12237484"],
+									["캐논 EOS 550D", "https://prod.danawa.com/info/?pcode=1017339&keyword=%EC%BA%90%EB%85%BC+EOS+550D&cate=12237484"],
+								]
+							}
+						}
+					]
+				}
+			],
+				{
+					},
+					skills: [
+						{
+							name: "Shadow Parts",
+							description: {
+								text: "Learn how to style the inside of a shadow tree from outside of that Shadow Tree by using Shadow Parts.",
+								links: [
+									["Fergald - CSS Shadow ::part and ::theme", "https://github.com/fergald/docs/blob/master/explainers/css-shadow-parts-1.md"],
+									["MDN - ::part", "https://developer.mozilla.org/en-US/docs/Web/CSS/::part"],
+									["W3C - CSS Shadow Parts", "https://www.w3.org/TR/css-shadow-parts-1/"],
+									["Monica - ::part and ::theme, an explainer", "https://meowni.ca/posts/part-theme-explainer/"],
+									["Youtube - CSS Shadow Parts", "https://www.youtube.com/watch?v=yVrvOueeb3s"],
+									["Chrome Status - ::part and ::theme", "https://www.chromestatus.com/features/5763933658939392"]
+								]
+							}
+						},
+						{
+							name: "Slots",
+							description: {
+								text: "Learn how to compose Custom Elements by using slots.",
+								links: [
+									["Alligator.io - Composing Custom Elements", "https://alligator.io/web-components/composing-slots-named-slots/"],
+									["Javascript.info - Slots Composition", "https://javascript.info/slots-composition"],
+									["MDN - HTML <slot> element", "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/slot"],
+									["Component.kitchen - A history of the HTML slot element", "https://component.kitchen/blog/posts/a-history-of-the-html-slot-element"],
+								]
+							}
+						},
+						{
+							name: "Best practices",
+							description: {
+								text: "Get familiar with best practices when it comes to building Web Components.",
+								links: [
+									["Google Devs - Custom Element Best Practices", "https://developers.google.com/web/fundamentals/web-components/best-practices"],
+									["Webcomponents.org - Web Components Best Practices", "https://www.webcomponents.org/community/articles/web-components-best-practices"],
+									["mateusortiz - Web Components the Right Way", "https://github.com/mateusortiz/webcomponents-the-right-way"],
+									["dev.to - Making Web Components for Different Contexts", "https://dev.to/equinusocio/making-web-components-for-different-contexts-25ed"]
+								]
+							}
+						}		
+					]
 				},
 				{
-					name: "Shadow DOM",
+					name: "야외촬영",
 					description: {
 						text: "Learn how to encapsulate your CSS using Shadow DOM.",
 						links: [
@@ -70,20 +135,20 @@ export const webcomponentsCollection = {
 									["Component.kitchen - A history of the HTML slot element", "https://component.kitchen/blog/posts/a-history-of-the-html-slot-element"],
 								]
 							}
-						}
+						},
+						{
+							name: "Best practices",
+							description: {
+								text: "Get familiar with best practices when it comes to building Web Components.",
+								links: [
+									["Google Devs - Custom Element Best Practices", "https://developers.google.com/web/fundamentals/web-components/best-practices"],
+									["Webcomponents.org - Web Components Best Practices", "https://www.webcomponents.org/community/articles/web-components-best-practices"],
+									["mateusortiz - Web Components the Right Way", "https://github.com/mateusortiz/webcomponents-the-right-way"],
+									["dev.to - Making Web Components for Different Contexts", "https://dev.to/equinusocio/making-web-components-for-different-contexts-25ed"]
+								]
+							}
+						}		
 					]
-				},
-				{
-					name: "Best practices",
-					description: {
-						text: "Get familiar with best practices when it comes to building Web Components.",
-						links: [
-							["Google Devs - Custom Element Best Practices", "https://developers.google.com/web/fundamentals/web-components/best-practices"],
-							["Webcomponents.org - Web Components Best Practices", "https://www.webcomponents.org/community/articles/web-components-best-practices"],
-							["mateusortiz - Web Components the Right Way", "https://github.com/mateusortiz/webcomponents-the-right-way"],
-							["dev.to - Making Web Components for Different Contexts", "https://dev.to/equinusocio/making-web-components-for-different-contexts-25ed"]
-						]
-					}
 				},
 				{
 					name: "Constructible Stylesheets",
@@ -99,6 +164,101 @@ export const webcomponentsCollection = {
 						]
 					}
 				},
+				{
+					name: "Form Participation",
+					description: {
+						text: "Learn how the Form Participation API proposal enables elements, other than built-in form control elements, to participate in form submission and validation.",
+						links: [
+							["Form Participation API Explained", "https://docs.google.com/document/d/1JO8puctCSpW-ZYGU8lF-h4FWRIDQNDVexzHoOQ2iQmY/edit"],
+							["Chrome Status - Form-associated custom elements", "https://www.chromestatus.com/features/4708990554472448"],
+							["web-platform-tests - Form Participation", "https://github.com/web-platform-tests/wpt/tree/master/custom-elements/form-associated"],
+							["Google Devs - Form-associated custom elements", "https://developers.google.com/web/updates/2019/09/nic77#form-associated_custom_elements"],
+							["web.dev - More capable form controls", "https://web.dev/more-capable-form-controls/"],
+							["W3C - Creating a form-associated custom element", "https://html.spec.whatwg.org/multipage/custom-elements.html#custom-elements-face-example"],
+						]
+					}
+				}
+			]
+		},
+		{
+			name: "조명기기",
+			skills: [
+				{
+					name: "Form Participation",
+					description: {
+						text: "Learn how the Form Participation API proposal enables elements, other than built-in form control elements, to participate in form submission and validation.",
+						links: [
+							["Form Participation API Explained", "https://docs.google.com/document/d/1JO8puctCSpW-ZYGU8lF-h4FWRIDQNDVexzHoOQ2iQmY/edit"],
+							["Chrome Status - Form-associated custom elements", "https://www.chromestatus.com/features/4708990554472448"],
+							["web-platform-tests - Form Participation", "https://github.com/web-platform-tests/wpt/tree/master/custom-elements/form-associated"],
+							["Google Devs - Form-associated custom elements", "https://developers.google.com/web/updates/2019/09/nic77#form-associated_custom_elements"],
+							["web.dev - More capable form controls", "https://web.dev/more-capable-form-controls/"],
+							["W3C - Creating a form-associated custom element", "https://html.spec.whatwg.org/multipage/custom-elements.html#custom-elements-face-example"],
+						]
+					}
+				}
+			]
+		},
+		{
+			name: "마이크",
+			skills: [
+				{
+					name: "Form Participation",
+					description: {
+						text: "Learn how the Form Participation API proposal enables elements, other than built-in form control elements, to participate in form submission and validation.",
+						links: [
+							["Form Participation API Explained", "https://docs.google.com/document/d/1JO8puctCSpW-ZYGU8lF-h4FWRIDQNDVexzHoOQ2iQmY/edit"],
+							["Chrome Status - Form-associated custom elements", "https://www.chromestatus.com/features/4708990554472448"],
+							["web-platform-tests - Form Participation", "https://github.com/web-platform-tests/wpt/tree/master/custom-elements/form-associated"],
+							["Google Devs - Form-associated custom elements", "https://developers.google.com/web/updates/2019/09/nic77#form-associated_custom_elements"],
+							["web.dev - More capable form controls", "https://web.dev/more-capable-form-controls/"],
+							["W3C - Creating a form-associated custom element", "https://html.spec.whatwg.org/multipage/custom-elements.html#custom-elements-face-example"],
+						]
+					}
+				}
+			]
+		},
+		{
+			name: "조명기기",
+			skills: [
+				{
+					name: "Form Participation",
+					description: {
+						text: "Learn how the Form Participation API proposal enables elements, other than built-in form control elements, to participate in form submission and validation.",
+						links: [
+							["Form Participation API Explained", "https://docs.google.com/document/d/1JO8puctCSpW-ZYGU8lF-h4FWRIDQNDVexzHoOQ2iQmY/edit"],
+							["Chrome Status - Form-associated custom elements", "https://www.chromestatus.com/features/4708990554472448"],
+							["web-platform-tests - Form Participation", "https://github.com/web-platform-tests/wpt/tree/master/custom-elements/form-associated"],
+							["Google Devs - Form-associated custom elements", "https://developers.google.com/web/updates/2019/09/nic77#form-associated_custom_elements"],
+							["web.dev - More capable form controls", "https://web.dev/more-capable-form-controls/"],
+							["W3C - Creating a form-associated custom element", "https://html.spec.whatwg.org/multipage/custom-elements.html#custom-elements-face-example"],
+						]
+					}
+				}
+			]
+		},
+		{
+			name: "컴퓨터",
+			skills: [
+				{
+					name: "Form Participation",
+					description: {
+						text: "Learn how the Form Participation API proposal enables elements, other than built-in form control elements, to participate in form submission and validation.",
+						links: [
+							["Form Participation API Explained", "https://docs.google.com/document/d/1JO8puctCSpW-ZYGU8lF-h4FWRIDQNDVexzHoOQ2iQmY/edit"],
+							["Chrome Status - Form-associated custom elements", "https://www.chromestatus.com/features/4708990554472448"],
+							["web-platform-tests - Form Participation", "https://github.com/web-platform-tests/wpt/tree/master/custom-elements/form-associated"],
+							["Google Devs - Form-associated custom elements", "https://developers.google.com/web/updates/2019/09/nic77#form-associated_custom_elements"],
+							["web.dev - More capable form controls", "https://web.dev/more-capable-form-controls/"],
+							["W3C - Creating a form-associated custom element", "https://html.spec.whatwg.org/multipage/custom-elements.html#custom-elements-face-example"],
+						]
+					}
+				}
+			]
+		},
+		{
+			name: "기타",
+			skills: [
 				{
 					name: "Form Participation",
 					description: {
