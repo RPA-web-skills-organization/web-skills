c8e65dec98030ce1dcb0ac0b6b1fe67f2e606486
Lanplica <rosenkranz723@gmail>
2022-11-06 19:25:55 +0900

    3번 각 항목 정보링크 추가 및 컴퓨터, 캡쳐보드 항목 추가(#23)

--- src/data/web-components.js
+++ src/data/web-components.js
@@ -9,6 +9,7 @@ export const webcomponentsCollection = {
 					description: {
 						text: "Web과 Camera의 합성어로, 주로 PC와 연결하여 화상 데이터를 네트워크 상에 스트리밍할 목적으로 개발된 카메라를 말한다.",
 						links: [
+							["정보글", "https://lite-life.tistory.com/entry/%EB%B0%A9%EC%86%A1%EC%9A%A9-%EC%BA%A0-%EC%B6%94%EC%B2%9C-%EC%88%9C%EC%9C%84-Top-7"],
 							["프리에이티브 AF500FHD", "https://www.coupang.com/vp/products/5267282638?itemId=7498448512&src=1139000&spec=10799999&addtag=400&ctag=5267282638&lptag=AF5411078&itime=20221101224645&pageType=PRODUCT&pageValue=5267282638&wPcid=16672917945289351501072&wRef=lite-life.tistory.com&wTime=20221101224645&redirect=landing&traceid=V0-101-71102f6b35910cae&placementid=&clickBeacon=&campaignid=&contentcategory=&imgsize=&pageid=&deviceid=&token=&contenttype=&subid=tistoryblog&impressionid=&campaigntype=&requestid=&contentkeyword=&subparam=&isAddedCart="],
 							["앱코 APC930U", "https://www.coupang.com/vp/products/1634788413?itemId=2788295532&src=1139000&spec=10799999&addtag=400&ctag=1634788413&lptag=AF5411078&itime=20221101224939&pageType=PRODUCT&pageValue=1634788413&wPcid=16672917945289351501072&wRef=lite-life.tistory.com&wTime=20221101224939&redirect=landing&traceid=V0-101-f5db09344ecd5cfa&placementid=&clickBeacon=&campaignid=&contentcategory=&imgsize=&pageid=&deviceid=&token=&contenttype=&subid=tistoryblog&impressionid=&campaigntype=&requestid=&contentkeyword=&subparam=&isAddedCart="],
 							["앤커 파워컨퍼런스 C300 A3361", "https://www.coupang.com/vp/products/6199153837?itemId=12289074366&src=1139000&spec=10799999&addtag=400&ctag=6199153837&lptag=AF5411078&itime=20221101225140&pageType=PRODUCT&pageValue=6199153837&wPcid=16672917945289351501072&wRef=lite-life.tistory.com&wTime=20221101225140&redirect=landing&traceid=V0-101-9d568a4bbbdd278f&placementid=&clickBeacon=&campaignid=&contentcategory=&imgsize=&pageid=&deviceid=&token=&contenttype=&subid=tistoryblog&impressionid=&campaigntype=&requestid=&contentkeyword=&subparam=&isAddedCart="],
@@ -21,6 +22,7 @@ export const webcomponentsCollection = {
 							description: {
 								text: "일반 카메라보다 센서 크기를 키워 화질을 높이고, 이에 더해 비교적 큰 렌즈와 수동 조작 기능 등을 추가한 전문가급 디지털카메라",
 								links: [
+									["정보글", "https://plan.danawa.com/info/index.php?nPlanSeq=1528"],
 									["캐논 파워샷 G7 X Mark II", "https://prod.danawa.com/info/?pcode=3799052"],
 									["파나소닉 루믹스 DMC-LX10", "https://prod.danawa.com/info/?pcode=4703553"],
 									["SONY 사이버샷 DSC-RX100 VI", "https://prod.danawa.com/info/?pcode=6199512"]
@@ -32,6 +34,7 @@ export const webcomponentsCollection = {
 							description: {
 								text: "렌즈 교환, 디스플레이 장치와 라이브 뷰 촬영을 기반으로 하는 형태의 시스템 카메라를 통틀어 부르는 명칭이다.",
 								links: [
+									["정보글", "https://plan.danawa.com/info/index.php?nPlanSeq=1528"],
 									["SONY 알파 A6400", "https://prod.danawa.com/info/?pcode=7213111"],
 									["캐논 EOS M50", "https://prod.danawa.com/info/?pcode=5976045"],
 									["파나소닉 루믹스 DMC-GX85", "https://prod.danawa.com/info/?pcode=4087856"]
@@ -43,6 +46,7 @@ export const webcomponentsCollection = {
 							description: {
 								text: "디지털 일안(단일 렌즈) 반사식 카메라(Digital Single-Lens Reflex Camera), 줄여서 DSLR 카메라는 카메라의 렌즈를 통해 인식 장치에 맺히는 피사체의 모습과 거울을 통해 뷰파인더로 반사되어 촬영자에게 보이는 모습이 동일한 디지털카메라",
 								links: [
+									["정보글", "https://www.youtube.com/watch?v=47P88xS2_AI"],
 									["캐논 EOS 5D Mark IV", "https://prod.danawa.com/info/?pcode=3815381&keyword=%EC%BA%90%EB%85%BC+EOS+5D+%EB%A7%88%ED%81%AC+IV+DSLR&cate=12237484"],
 									["니콘 D3500", "https://prod.danawa.com/info/?pcode=6495760&keyword=%EB%8B%88%EC%BD%98+D3500&cate=12237484"],
 									["캐논 EOS 550D", "https://prod.danawa.com/info/?pcode=1017339&keyword=%EC%BA%90%EB%85%BC+EOS+550D&cate=12237484"],
@@ -56,6 +60,7 @@ export const webcomponentsCollection = {
 					description: {
 						text: "마이크로폰(Microphone), 줄여서 마이크(Mic.)는 음향을 전기 신호로 변환하는 장치로, 센서 및 변환기(Transducer)의 일종",
 						links: [
+							["정보글", "https://proguide.co.kr/best-microphone/"],
 							["로지텍 블루 마이크로폰 YETI X", "https://prod.danawa.com/info/?pcode=11897086&keyword=%EB%A1%9C%EC%A7%80%ED%85%8D+%EB%B8%94%EB%A3%A8+%EB%A7%88%EC%9D%B4%ED%81%AC%EB%A1%9C%ED%8F%B0+YETI+X&cate=1034314"],
 							["HyperX QuadCast S", "https://prod.danawa.com/info/?pcode=12902456&keyword=%ED%95%98%EC%9D%B4%ED%8D%BC%EC%97%91%EC%8A%A4+QuadCast+%EA%B2%8C%EC%9D%B4%EB%B0%8D+%EB%A7%88%EC%9D%B4%ED%81%AC&cate=1034314"],
 							["Razer SEIREN MINI", "https://prod.danawa.com/info/?pcode=13197293&keyword=Razer+Seiren+Mini+%EB%A0%88%EC%9D%B4%EC%A0%80+%EB%A7%88%EC%9D%B4%ED%81%AC&cate=1034314"],
@@ -69,6 +74,7 @@ export const webcomponentsCollection = {
 							description: {
 								text: "자율 감각 쾌락 반응(Autonomous Sensory Meridian Response, ASMR)은 주로 청각을 중심으로 하는 시각적, 청각적, 촉각적, 후각적, 혹은 인지적 자극에 반응하여 나타나는, 형언하기 어려운 심리적 안정감이나 쾌감 따위의 감각적 경험을 일으킬 수 있는 마이크",
 								links: [
+									["정보글", "https://gongrg.tistory.com/221"],
 									["ZOOM H1N", "https://prod.danawa.com/info/?pcode=6415397&keyword=ZOOM+H1N&cate=12237393"],
 									["TASCAM DR-40X", "https://prod.danawa.com/info/?pcode=7377943&keyword=%ED%83%80%EC%8A%A4%EC%BA%A0&cate=12237393"],
 									["3DIO Free Space", "https://www.coupang.com/vp/products/6112955104?itemId=11533388687&vendorItemId=78808459563&q=3DIO+Free+Space&itemsCount=36&searchId=13bd6332b5134c4095bdc0e45c3cfa5c&rank=1&isAddedCart="],
@@ -84,9 +90,57 @@ export const webcomponentsCollection = {
 					description: {
 						text: "조명의 종류와 배치에 따른 효과가 다름.",
 						links: [
+							["정보글", "https://qoowoona.com/%EB%B0%A9%EC%86%A1%EC%A1%B0%EB%AA%85-20220727/"],
 							["코멧 탁상용 링라이트", "https://www.coupang.com/vp/products/2145215943?itemId=3642565408&vendorItemId=71628088174&src=1032034&spec=10305201&addtag=400&ctag=2145215943&lptag=I3642565408&itime=20221102000220&pageType=PRODUCT&pageValue=2145215943&wPcid=16672917945289351501072&wRef=prod.danawa.com&wTime=20221102000220&redirect=landing&isAddedCart="],
 							["유쾌한생각 PL-2SET-43LH", "https://prod.danawa.com/info/?pcode=7298734&keyword=%EC%9C%A0%EC%BE%8C%ED%95%9C%EC%83%9D%EA%B0%81+%EB%A3%A9%EC%8A%A4%ED%8C%A8%EB%93%9C43H+LED%EC%A1%B0%EB%AA%85&cate=12237571"],
 							["한빛랜드 큐브라이트33 LED조명", "https://prod.danawa.com/info/?pcode=15775376&keyword=%ED%81%90%EB%B8%8C%EB%9D%BC%EC%9D%B4%ED%8A%B8+led+%EC%A1%B0%EB%AA%85&cate=12237571"]
+						]
+					}
+				},
+				{
+					name: "컴퓨터",
+					description: {
+						text: "방송용 컴퓨터로 사용하기 위한 부품 중 가장 핵심 부품인 CPU와 GPU의 최소사양을 소개",
+						links: [
+							["정보글1", "http://shop.danawa.com/shopmain/?controller=goods&methods=index&categorySeq=5176"],
+							["정보글2", "https://gall.dcinside.com/mgallery/board/view/?id=xsplitobs&no=14&search_head=30&page=1"],
+							["CPU 랭킹사이트", "https://www.cpubenchmark.net/high_end_cpus.html"],
+							["GPU 랭킹사이트", "https://www.videocardbenchmark.net/high_end_gpus.html"]
+						]
+					},
+					skills: [
+						{
+							name: "송출용 컴퓨터",
+							description: {
+								text: "방송과 게임을 하나의 컴퓨터로 하려면 다소 사양이 높아야 하므로 방송 송출용 컴퓨터의 최소사양만 다룸",
+								links: [
+									["CPU : 인텔 코어i3-8세대 8100", "https://prod.danawa.com/info/?pcode=5530456&keyword=%EC%9D%B8%ED%85%94+8%EC%84%B8%EB%8C%80+i3&cate=113973"],
+									["GPU : 지포스 GTX960", "https://prod.danawa.com/info/?pcode=6419976&keyword=%EC%A7%80%ED%8F%AC%EC%8A%A4+gtx+960&cate=112753"]
+								]
+							}
+						},
+						{
+							name: "게임용 컴퓨터",
+							description: {
+								text: "게임용 컴퓨터를 따로 구비할 시 게임이 멈춰도 방송은 끊기지 않는 등의 대처 가능 및 다양한 게임이 가능. ",
+								links: [
+									["CPU : 인텔 코어i7-9세대 9700KF", "https://prod.danawa.com/info/?pcode=7517686&keyword=%EC%9D%B8%ED%85%94+i7-9%EC%84%B8%EB%8C%80&cate=113973"],
+									["GPU : 지포스 GTX1060", "https://prod.danawa.com/info/?pcode=10310937&keyword=gtx+1060&cate=112753#bookmark_cm_opinion"]
+								]
+							}
+						}
+					]
+				},
+				{
+					name: "캡쳐보드",
+					description: {
+						text: "영상 신호를 실시간으로 받아 인코딩을 거쳐 저장장치에 저장하는 주변기기이다.",
+						links: [
+							["정보글1", "https://www.drgo.pro/bbs/board.php?bo_table=lec&wr_id=28"],
+							["정보글2", "https://itrvrl.com/%EC%BA%A1%EC%B3%90%EB%B3%B4%EB%93%9C-%EC%B6%94%EC%B2%9C/"],
+							["AVerMedia Live Gamer ULTRA", "https://prod.danawa.com/info/?pcode=6275042&keyword=%EC%97%90%EB%B2%84%EB%AF%B8%EB%94%94%EC%96%B4+%EC%BA%A1%EC%B2%98%EB%B3%B4%EB%93%9C+GC553&cate=112810"],
+							["ASUS TUF Gaming CAPTURE BOX-CU4K30", "https://prod.danawa.com/info/?pcode=14157470&keyword=%EC%97%90%EC%9D%B4%EC%88%98%EC%8A%A4+%EC%BA%A1%EC%B3%90%EB%B3%B4%EB%93%9C&cate=112810"],
+							["스카이디지탈 슈퍼캐스트 X LIVE 2K", "https://prod.danawa.com/info/?pcode=12830987&keyword=%EC%8A%A4%EC%B9%B4%EC%9D%B4%EB%94%94%EC%A7%80%ED%83%88+%EC%8A%88%ED%8D%BC%EC%BA%90%EC%8A%A4%ED%8A%B8+%EC%BA%A1%EC%B3%90%EC%B9%B4%EB%93%9C+SUPERCAST+X+LIVE+2K&cate=112810"]
 						]
 					}
 				}								
